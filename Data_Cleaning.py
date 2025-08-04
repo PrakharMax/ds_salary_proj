e8a550f347cce77ce1b9852fa8f96a506f4ce91f
Prakash Prakhar <prakhar.prakash@aalto.fi>
2025-08-04 14:44:03 +0300

    Update with data cleaning
    All points covered

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -9,9 +9,10 @@ import pandas as pd
 df=pd.read_csv('glassdoor_jobs_fixed(700+ records).csv')
 
 #salary parsing, remove nan
-#remove empty nan
 #show the states in the dictionary
 
+
+#salary parsing, remove nan
 df=df[df['salary'].notna()]
 
 df['salary']=df['salary'].str.split('(').str[0]
@@ -25,4 +26,16 @@ df['max_sal'] = df.apply(lambda row: row['min_sal'] if pd.isna(row['max_sal']) e
 df['min_sal']=pd.to_numeric(df['min_sal'])
 df['max_sal']=pd.to_numeric(df['max_sal'])
 df['avg_salary'] = (df['min_sal'] + df['max_sal']) / 2
-print(df.info())
+
+#ratings
+df['rating']=df['rating'].fillna(-1)
+
+#States segregation
+
+df['city']=df['location'].str.split(',').str[0]
+df['state']=df['location'].str.split(',').str[1]
+#df['state']=df['state'].apply(lambda x: x['state'] if x.notna() else 'Remote')
+#df['state']=df['state'].fillna('Remote')
+df['state'] = df.apply(lambda row: row['state'] if pd.notna(row['state']) else 'Remote', axis=1)
+#print(df.state.value_counts())
+#print(df.info())
